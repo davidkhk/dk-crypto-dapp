37c45e726eb5e109dbc40673d18664e600dd08c6
David <davidkhk@gmail.com>
2022-02-02 07:41:16 -0300

    Finish Services sections

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = PROCESS.ENV.REACT_APP_CONTRACT_ADDRESS;
+export const contractAddress = '0x41B81Ae25D373d180A82b7Bb9A955AF6873beD37';
