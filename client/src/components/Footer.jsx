d04b1952267b7c322793a577af267d56748250cb
David <davidkhk@gmail.com>
2022-02-02 15:51:55 -0300

    Fix Footer section

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -16,11 +16,11 @@ const Footer = () => {
             </div>
             <div className="flex justify-center items-center flex-col mt-5">
                 <p className="text-white text-sm text-center">Come join us</p>
-                <p className="text-white text-sm text-center">davidkhk@gmail.com</p>
+                <p className="text-white text-sm text-center">hello@davidkang.me</p>
             </div>
             <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5" />
             <div className="sm:[90%] w-full flex justify-between items-center mt-3">
-                <p className="text-white text-sm text-center">@davidkhk 2022</p>
+                <p className="text-white text-sm text-center">Designed and built by David Kang | 2022</p>
                 <p className="text-white text-sm text-center">All rights reserved</p>
             </div>
         </div>
